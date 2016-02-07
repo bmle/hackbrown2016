59907f9a7574814dfa0d5a4f9ddb45e9e03eea6e
Kimberly Le <le_kimberly@rocketmail.com>
2016-02-07 09:38:31 -0500

    Remove menus from layouts that dont need it

--- app/src/main/java/bmle/hackatbrown2016/Feed.java
+++ app/src/main/java/bmle/hackatbrown2016/Feed.java
@@ -3,8 +3,11 @@ package bmle.hackatbrown2016;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Feed extends ActionBarActivity {
 
@@ -21,6 +24,32 @@ public class Feed extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_feed, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.menu_settings:
+                Toast.makeText(getApplicationContext(), "Settings clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_switch:
+                Toast.makeText(getApplicationContext(), "Switch modes activated", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
