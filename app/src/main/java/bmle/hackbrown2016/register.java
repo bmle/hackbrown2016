f2a61164a003a21c83bb3472a2a0d2d22982d4a4
fchiem <FrankChiem@Franks-MacBook-Pro-2.local>
2016-02-06 21:24:47 -0500

    Login Page
    Making the login in page

---
+++ app/src/main/java/bmle/hackbrown2016/register.java
@@ -0,0 +1,14 @@
+package bmle.hackbrown2016;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class register extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
