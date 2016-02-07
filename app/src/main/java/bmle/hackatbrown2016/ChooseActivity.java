c6e13ecf1557a4646f44f8974f28ffce7058d30b
fchiem <FrankChiem@Franks-MacBook-Pro-2.local>
2016-02-07 02:13:01 -0500

    Transition to choose class

---
+++ app/src/main/java/bmle/hackatbrown2016/ChooseActivity.java
@@ -0,0 +1,14 @@
+package bmle.hackatbrown2016;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ChooseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+
+}
--- app/src/main/java/bmle/hackatbrown2016/SignUpActivity.java
+++ app/src/main/java/bmle/hackatbrown2016/SignUpActivity.java
@@ -1,9 +1,12 @@
 package bmle.hackatbrown2016;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SignUpActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class SignUpActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button choose = (Button) findViewById(R.id.bRegister);
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, ChooseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
